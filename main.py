5bf2e54019c55eb3fce77e183fa81c27ce3631ea
cs-sohan <cs.sohanchakraborty@gmail.com>
2026-02-17 00:05:12 +0530

    [Feat.]: Used Dict storage to reduce CRUD operations time complexity from O(n) to O(1)

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from uuid import UUID
 
 app = FastAPI()
 
-products = [
+products : dict[UUID,Product] = {}
+
+initial_products = [
     Product(name='IPhone',description="Apple Iphone",price=999,quantity=10),
     Product(name='Laptop',description="Good Quality laptop",price=1098,quantity=7),
     Product(name='Chair',description="Good Chair",price=36,quantity=58),
@@ -12,30 +14,33 @@ products = [
     Product(name='Gun',description="",price=999,quantity=1),
 ]
 
+for product in initial_products:
+    products[product.id] = product
+
 @app.get("/")
-def greet():
+def home():
     return {"message":"Hello, Welcome to the application!"}
 
 @app.get("/products",response_model=list[Product],tags=["Products"])
 def get_all_products():
-    return products
+    return list(products.values())
 
 @app.get("/products/{product_id}",response_model=Product,tags=["Products"])
 def get_product_by_id(product_id: UUID):
-    product = next((p for p in products if p.id == product_id), None)
+    product = products.get(product_id)
     if product is None:
-        raise HTTPException(status_code=404, detail="Product Not found!")
+        raise HTTPException(status_code=404, detail="Product Not Found!")
     return product
 
 @app.post("/products",response_model=Product,status_code=201,tags=["Products"])
 def add_product(product:ProductCreate):
     new_product = Product(**product.model_dump())
-    products.append(new_product)
+    products[new_product.id] = new_product
     return new_product
 
 @app.patch("/products/{product_id}",response_model=Product,tags=["Products"])
 def update_product(product_id:UUID, product_update:ProductUpdate):
-    existing_product = next((p for p in products if p.id == product_id),None)
+    existing_product = products.get(product_id)
     if existing_product is None:
         raise HTTPException(status_code=404,detail='Product Not Found!')
     product_changes = product_update.model_dump(exclude_unset=True)
@@ -45,9 +50,6 @@ def update_product(product_id:UUID, product_update:ProductUpdate):
 
 @app.delete("/products/{product_id}",status_code=204,tags=["Products"])
 def delete_product(product_id:UUID):
-    for index,product in enumerate(products):
-        if product.id == product_id:
-            del products[index]
-            return
-    raise HTTPException(status_code=404,detail='Product Not Found!')
-    
+    deleted_product = products.pop(product_id,None)
+    if deleted_product is None:
+        raise HTTPException(status_code=404,detail='Product Not Found!')    
